30a57d9303bdba8b67b871ef153dee4a62ff7b55
tchuanromanee <tchuanromanee@yahoo.com>
2017-01-24 20:12:01 -0500

    Added more lines to example1

--- Teaching_Materials/Week_1/Exercises/10_5_15/Example1.java
+++ Teaching_Materials/Week_1/Exercises/10_5_15/Example1.java
@@ -3,5 +3,6 @@ public class Example1 // Class header
    public static void main(String [] args) // Method header
    {
       System.out.println("Well, well, well, what have we here");
+      System.out.println("A sentence printed in a new line");
    }
 }
