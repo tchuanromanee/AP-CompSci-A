80441031c74a698442953a8ef77261f5b002dfbb
tchuanromanee <tchuanromanee@yahoo.com>
2017-02-05 17:46:24 -0500

    Added clarifying comments and changed structure a little

--- Teaching_Materials/Week_1/Exercises/10_8_15/Exercise1.java
+++ Teaching_Materials/Week_1/Exercises/10_8_15/Exercise1.java
@@ -1,15 +1,19 @@
-public class Exercise1
-{
-   public static void main(String [] args)
-   {
-   int a = -3;
-   System.out.println(((7+(6*4))/(8-(3%2))+ ((17-(4/3))/(6+(3*2)))/(8-(7%4))));
+public class Exercise1 {
+   public static void main(String [] args) {
    
+   // Trying out a mathematical operation
+   System.out.println(((7+(6*4))/(8-(3%2))+ ((17-(4/3))/(6+(3*2)))/(8-(7%4))));
+   // Using spaces makes everything easier to read
    System.out.println( (((7+6*4)/(8-3%2)) + ((17-4/3)/(6+3*2))) / (8-7%4));
    
+   // Guess what is the answer
    System.out.println(7 + 6/4 - 3*7%2 + 5/2);
+   
+   // Are these 2 the same answers?
    System.out.println( 6 + 3/2.0 - 6/4 + 8%10);
    System.out.println( 6 + 4/2.0 - 6/4 + 8%10);
+   
+   int a = -3;
    String s = "abc";
    String t = 6 - 8 % 3 + s + "\n" + -a + 7 / 4;
    System.out.println(t);
